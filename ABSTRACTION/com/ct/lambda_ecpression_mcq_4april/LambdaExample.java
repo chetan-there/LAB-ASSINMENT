b35ac010d2c64d18ac49b32c7877ea4dcbb3e707
chetan-there <therechetan24@gmail.com>
2025-04-04 22:42:16 +0530

    lambda with method body

---
+++ ABSTRACTION/com/ct/lambda_ecpression_mcq_4april/LambdaExample.java
@@ -0,0 +1,66 @@
+package com.ct.lambda_ecpression_mcq_4april;
+
+// Functional interface with different data types
+interface IntOperation {
+    int operate(int a);
+}
+
+interface FloatOperation {
+    float operate(float a);
+}
+
+interface DoubleOperation {
+    double operate(double a);
+}
+
+interface BooleanOperation {
+    boolean operate(boolean a);
+}
+
+interface StringOperation {
+    String operate(String s);
+}
+
+// Multi-argument functional interfaces
+interface IntAdd {
+    int add(int a, int b);
+}
+
+interface StringConcat {
+    String concat(String a, String b);
+}
+
+public class LambdaExample {
+    public static void main(String[] args) {
+
+        // ---------- INT ----------
+        // Traditional Way
+        // IntOperation intOp = new IntOperation() {
+        //     public int operate(int a) {
+        //         return a * a;
+        //     }
+        // };
+
+        // Lambda Way
+        IntOperation intOp = a -> a * a;
+        System.out.println("Square of 5: " + intOp.operate(5)); // Output: 25
+
+        // ---------- FLOAT ----------
+        FloatOperation floatOp = f -> f + 3.5f;
+        System.out.println("Float calc: " + floatOp.operate(6.5f)); // Output: 10.0
+
+        // ---------- DOUBLE ----------
+        DoubleOperation doubleOp = d -> d / 2.0;
+        System.out.println("Double calc: " + doubleOp.operate(8.0)); // Output: 4.0
+
+        // ---------- BOOLEAN ----------
+        BooleanOperation boolOp = b -> !b;
+        System.out.println("Negation of true: " + boolOp.operate(true)); // Output: false
+
+        // ---------- STRING ----------
+        StringOperation stringOp = s -> s.toUpperCase();
+        System.out.println("Uppercase: " + stringOp.operate("lambda")); // Output: LAMBDA
+
+        //multi
+    }
+}
