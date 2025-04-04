afffac9c60dc74e51ec0eee46b54964b91c7e261
chetan-there <therechetan24@gmail.com>
2025-04-04 22:36:32 +0530

    notes & program on lambda

---
+++ ABSTRACTION/com/ct/lambda_ecpression_mcq_4april/Main.java
@@ -0,0 +1,16 @@
+package com.ct.lambda_ecpression_mcq_4april;
+
+// Lambda Expressions
+
+// interface Car {
+
+//     public void BMW();
+// }
+// public class Main{
+// public static void main(String[] args) {  // rv= referance variable 
+//     Car rv =/* public void BMW */ (/* parameters */) -> /*this is method body it can contains return */ System.err.println("bmw method");
+//     rv.BMW();
+// }
+// }
+
+
