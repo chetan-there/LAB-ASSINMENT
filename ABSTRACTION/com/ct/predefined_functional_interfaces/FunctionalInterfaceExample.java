a9d2a0323bb7511073bba42d6d19c63ab7c62500
chetan-there <therechetan24@gmail.com>
2025-04-07 19:16:58 +0530

    formated answers

--- ABSTRACTION/com/ct/predefined_functional_interfaces/FunctionalInterfaceExample.java
+++ ABSTRACTION/com/ct/predefined_functional_interfaces/FunctionalInterfaceExample.java
@@ -3,12 +3,17 @@ package com.ct.predefined_functional_interfaces;
 @FunctionalInterface
 interface NIT
 {
-	String str(String nit);
+	String nit(String name);
 }
 public class FunctionalInterfaceExample
 {
 	public static void main(String[] args) {
-	NIT nit = (String n) -> n +"ab";
-	System.out.println(nit.str("hello"));
+		NIT nit = (String name) -> {
+			return name;
+		};
+		
+		NIT nit1 = (String name) -> name;
+		
+		System.out.println(nit.nit("Welcome to ")+nit1.nit("NARESHIT"));
 	}
 }
