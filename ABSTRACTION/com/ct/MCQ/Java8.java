10eea0419ae3def1f9ad3c122d1cff8ddf9f880d
chetan-there <therechetan24@gmail.com>
2025-04-08 16:49:58 +0530

    initial

---
+++ ABSTRACTION/com/ct/MCQ/Java8.java
@@ -0,0 +1,19 @@
+package com.ct.MCQ;
+
+interface Calculate
+{
+	int myinterface(int a, int b);
+}
+
+interface NIT
+{
+	String myInterface(String a);
+}
+public class Java8 {
+	public static void main(String[] args) {
+		Calculate cal = (a) -> a+5;
+		cal.myinterface(1, 5);
+		NIT nit = (a) -> {return a;};
+		System.out.println(nit.myInterface("NIT"));
+	}
+}  
