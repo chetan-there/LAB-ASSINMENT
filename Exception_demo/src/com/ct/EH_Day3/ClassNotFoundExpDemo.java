a14ea27dccd21d4308bfb75500109284017d266e
chetan-there <therechetan24@gmail.com>
2025-04-16 23:41:04 +0530

    EH_Day3

--- Exception_demo/src/com/ct/EH_Day3/ClassNotFoundExpDemo.java
+++ Exception_demo/src/com/ct/EH_Day3/ClassNotFoundExpDemo.java
@@ -1,8 +1,24 @@
 package com.ct.EH_Day3;
 
+import java.util.Scanner;
+
 public class ClassNotFoundExpDemo {
+    public ClassNotFoundExpDemo(String className) {
+        try {
+            Class<?> x = Class.forName(className);
+            System.out.println("Class " + className + " class is found  : " + x);
+        } catch (ClassNotFoundException e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the class name to load ");
+        String cName = sc.next();
+        new ClassNotFoundExpDemo(cName);
+        sc.close();
 
     }
 }
