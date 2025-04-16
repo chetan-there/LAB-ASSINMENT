9d63e96cc49cfae9ac5a62945d073ee74429476f
chetan-there <therechetan24@gmail.com>
2025-04-16 17:31:25 +0530

    EH_Day3

--- Exception_demo/src/com/ct/EH_Day3/AreaCalculator.java
+++ Exception_demo/src/com/ct/EH_Day3/AreaCalculator.java
@@ -1,12 +1,32 @@
 package com.ct.EH_Day3;
 
+import java.util.Scanner;
+
 public class AreaCalculator {
     
     public static double calculateArea(double length ,double width){
-       
-        
+        if(length > 0 && width > 0){
+        return length*width ;        
+        }
+        else throw new IllegalArgumentException();
     }
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of rectangle: ");
+        double a=sc.nextDouble();
+        System.out.println("Enter width of rectangle: ");
+        double b=sc.nextDouble();
+
+        try {
+            calculateArea(a,b);
+            System.out.println(calculateArea(a,b));
+        }catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentException caught: Dimensions must be positive: length="+a+", width="+b);
+        }
+    }
+
+   
 
 
 }
