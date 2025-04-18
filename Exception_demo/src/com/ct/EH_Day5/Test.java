0ef85ecae14f40935f780efef98994593494f7e9
chetan-there <therechetan24@gmail.com>
2025-04-18 17:13:06 +0530

    EH_Day5

---
+++ Exception_demo/src/com/ct/EH_Day5/Test.java
@@ -0,0 +1,21 @@
+package com.ct.EH_Day5;
+
+public class Test 
+{
+public static void main(String[] args) 
+{
+   try {
+       throw new java.io.IOException();
+   } 
+     catch (java.io.IOException exc) {
+       System.err.println("In catch IOException: "+exc.getClass());
+       throw new RuntimeException();
+   }
+     catch (Exception exc) {
+       System.err.println("In catch Exception: "+exc.getClass());
+   }
+     finally {
+       System.err.println("In finally");
+   }
+}
+}
