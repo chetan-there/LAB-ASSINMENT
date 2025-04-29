282becad393f15453fe67dcb055c93fbe94d5cc8
chetan-there <therechetan24@gmail.com>
2025-04-29 21:27:45 +0530

    MT_Day4

--- Multithreading/src/com/ct/MT_Day4/OLA/DriverAssignment.java
+++ Multithreading/src/com/ct/MT_Day4/OLA/DriverAssignment.java
@@ -1,13 +1,13 @@
 package com.ct.MT_Day4.OLA;
 
-public class DriverAssignment extends Thread{
+class DriverAssignment extends Thread {
     @Override
     public void run() {
-        super.run();
         try {
-        System.out.println("Driver found and assigned!");
-        } catch (Exception e) {
-            // TODO: handle exception
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("Driver found and assigned!");
     }
 }
