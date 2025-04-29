7846b75bb6a93e0c2d801d302f3f406031178480
chetan-there <therechetan24@gmail.com>
2025-04-29 21:26:08 +0530

    MT_Day4

--- Multithreading/src/com/ct/MT_Day4/OLA/RideRequest.java
+++ Multithreading/src/com/ct/MT_Day4/OLA/RideRequest.java
@@ -1,15 +1,14 @@
 package com.ct.MT_Day4.OLA;
 
-public class RideRequest extends Thread{
+class RideRequest extends Thread {
     @Override
     public void run() {
-        super.run();
         try {
-           System.out.println("User requested a ride.... User should wait for 2 seconds"); 
-           System.out.println("Searching for nearby drivers...");
-           
-        } catch (Exception e) {
-            
+            System.out.println("User requested a ride...");
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("Searching for nearby drivers...");
     }
 }
