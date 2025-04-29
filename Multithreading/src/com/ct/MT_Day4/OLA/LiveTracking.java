e80078bcfb72c1f0c51ae5c486daebf258befb04
chetan-there <therechetan24@gmail.com>
2025-04-29 21:28:42 +0530

    MT_Day4

--- Multithreading/src/com/ct/MT_Day4/OLA/LiveTracking.java
+++ Multithreading/src/com/ct/MT_Day4/OLA/LiveTracking.java
@@ -1,8 +1,16 @@
 package com.ct.MT_Day4.OLA;
 
-public class LiveTracking extends Thread{
+class LiveTracking extends Thread {
     @Override
     public void run() {
-        super.run();
+        try {
+            for (int i = 20; i <= 100; i += 20) {
+                System.out.println("Updating ride location... " + i + "% completed");
+                Thread.sleep(100);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Ride Completed");
     }
 }
