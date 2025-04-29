d7f7f472109e9f40397f23f6655e6662c67233aa
chetan-there <therechetan24@gmail.com>
2025-04-29 21:29:44 +0530

    MT_Day4

--- Multithreading/src/com/ct/MT_Day4/OLA/RatingSystem.java
+++ Multithreading/src/com/ct/MT_Day4/OLA/RatingSystem.java
@@ -1,13 +1,14 @@
 package com.ct.MT_Day4.OLA;
 
-public class RatingSystem extends Thread{
+class RatingSystem extends Thread {
     @Override
     public void run() {
-        super.run();
         try {
             System.out.println("Requesting user rating...");
-        } catch (Exception e) {
-            System.out.println(e);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        System.out.println("User rated the ride: 5 star");
     }
 }
