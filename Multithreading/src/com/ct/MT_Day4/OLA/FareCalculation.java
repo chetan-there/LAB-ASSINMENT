f85f17d894a9807a97f88bbcf6d11a0b98358d7a
chetan-there <therechetan24@gmail.com>
2025-04-29 21:28:10 +0530

    MT_Day4

--- Multithreading/src/com/ct/MT_Day4/OLA/FareCalculation.java
+++ Multithreading/src/com/ct/MT_Day4/OLA/FareCalculation.java
@@ -1,14 +1,14 @@
 package com.ct.MT_Day4.OLA;
 
-public class FareCalculation extends Thread{
+class FareCalculation extends Thread {
     @Override
     public void run() {
-        super.run();
         try {
             System.out.println("Calculating estimated fare...");
-        } catch (Exception e) {
-            System.out.println(e);
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
+        System.out.println("Estimated fare: 135.75 RS.");
     }
 }
