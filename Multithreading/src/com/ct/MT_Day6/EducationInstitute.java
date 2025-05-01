4f925556ad7728845aafe30ec3091b0501ceb38e
chetan-there <therechetan24@gmail.com>
2025-05-01 11:43:56 +0530

    MT_Day6

--- Multithreading/src/com/ct/MT_Day6/EducationInstitute.java
+++ Multithreading/src/com/ct/MT_Day6/EducationInstitute.java
@@ -2,20 +2,19 @@ package com.ct.MT_Day6;
 
 public class EducationInstitute {
 
-    String[] courses = new String[10];
+    Course[] courses;
+    Offer[] offers;
 
-    String[] offers = new String[10];
-
-    public EducationInstitute(String[] courses, String[] offers) {
+    public EducationInstitute(Course[] courses, Offer[] offers) {
         this.courses = courses;
         this.offers = offers;
     }
 
-    public String[] getCourses() {
+    public Course[] getCourses() {
         return courses;
     }
 
-    public String[] getOffers() {
+    public Offer[] getOffers() {
         return offers;
     }
 
--- Multithreading/src/com/ct/MT_Day6/EducationInstituteApp.java
+++ Multithreading/src/com/ct/MT_Day6/EducationInstituteApp.java
@@ -3,5 +3,11 @@ package com.ct.MT_Day6;
 //ELC
 
 public class EducationInstituteApp {
+
+    public static void main(String[] args) {
+        
+        Student s1= new Student("Virat", "");
+
+    }
     
 }
