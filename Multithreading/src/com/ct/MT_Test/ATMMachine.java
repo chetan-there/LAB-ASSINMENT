5011516be1bfcb2aaf53a309e31bf62e4df478ab
chetan-there <therechetan24@gmail.com>
2025-05-03 23:07:20 +0530

    MT_Test

--- Multithreading/src/com/ct/MT_Test/ATMMachine.java
+++ Multithreading/src/com/ct/MT_Test/ATMMachine.java
@@ -4,4 +4,16 @@ package com.ct.MT_Test;
 class Account {
     private int balance = 0;
 
-  
+    // Synchronized withdraw method
+    public synchronized void withdraw(int amount) {
+        while (balance < amount) {
+            System.out.println("Insufficient balance for withdrawal: " + amount + ". Waiting...");
+            try {
+                wait(); // Wait until deposit is made
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        balance -= amount;
+        System.out.println("Withdrawn: " + amount + " | Updated Balance: " + balance);
+    }
