af0474c775be2e24f00e23416c3fbba99d70f8f1
chetan-there <therechetan24@gmail.com>
2025-05-05 11:19:07 +0530

    initial

--- Multithreading/src/com/ct/MT_Day1/Mythread.java
+++ Multithreading/src/com/ct/MT_Day1/Mythread.java
@@ -1,5 +1,4 @@
 public class Mythread extends Thread {
-
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
--- Multithreading/src/com/ct/MT_Day1/myRunnable.java
+++ Multithreading/src/com/ct/MT_Day1/myRunnable.java
@@ -1,6 +1,6 @@
+
 public class myRunnable implements Runnable{
 
-    
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
