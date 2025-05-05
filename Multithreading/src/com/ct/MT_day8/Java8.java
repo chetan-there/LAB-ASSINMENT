06e08d019cb344401a4d1da458ea53872cdf7755
chetan-there <therechetan24@gmail.com>
2025-05-05 12:27:13 +0530

    mcq day8

--- Multithreading/src/com/ct/MT_day8/Java8.java
+++ Multithreading/src/com/ct/MT_day8/Java8.java
@@ -3,8 +3,8 @@ package com.ct.MT_day8;
 class NIT extends Thread {
 	@Override
 	public void run() {
-		System.out.println(currentThread().getId());
 		System.out.println(currentThread().getName());
+		System.out.println(currentThread().getPriority());
 	}
 }
 
@@ -14,6 +14,11 @@ public class Java8 {
 		NIT nit = new NIT();
 		NIT nit1 = new NIT();
 		NIT nit2 = new NIT();
+
+		nit.setPriority(Thread.MIN_PRIORITY);
+		nit1.setPriority(Thread.MIN_PRIORITY);
+		nit2.setPriority(Thread.MIN_PRIORITY);
+
 		nit.start();
 		nit1.start();
 		nit2.start();
