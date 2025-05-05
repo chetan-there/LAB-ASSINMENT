4f5201ee958c598cb2a8aa29e250e445fc11f810
chetan-there <therechetan24@gmail.com>
2025-05-05 12:13:46 +0530

    mcq day8

--- Multithreading/src/com/ct/MT_day8/Java8.java
+++ Multithreading/src/com/ct/MT_day8/Java8.java
@@ -1,8 +1,25 @@
 package com.ct.MT_day8;
 
-public class Java8 {
-	public static void main(String[] args) {
-		new Thread(()-> 
-		System.out.println("Naresh I Technologies")).start();
-    }
+public class Java8 extends Thread {
+
+	public void run() {
+		for (int a = 0; a < 5; a++) {
+			System.out.println("display");
+		}
+	}
+
+	public void display1() {
+		for (int a = 0; a < 5; a++) {
+			System.out.println("display1");
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Java8 java = new Java8();
+		java.start();
+		java.run();
+		java.wait();
+		java.display1();
+	}
 }
