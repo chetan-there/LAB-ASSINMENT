93676f855e7971d8c04cdb172aeea3a95876def9
chetan-there <therechetan24@gmail.com>
2025-05-08 12:04:43 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -1,8 +1,7 @@
 package com.ct.ST_day1;
 public class StringHandling {
-	public static void main(String[] args) 
-	{
-		String str = "  Java Programming  ";
-                System.out.println(str.trim());
+	public static void main(String[] args) {
+		String str = "NareshIT";
+		System.out.println(str.charAt(6));
 	}
 }
