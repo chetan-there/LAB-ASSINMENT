120ce137bb59579f7ca52ef7ff585b655a2c1b51
chetan-there <therechetan24@gmail.com>
2025-05-08 12:02:36 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -2,9 +2,7 @@ package com.ct.ST_day1;
 public class StringHandling {
 	public static void main(String[] args) 
 	{
-		String str = "apple,banana,orange";
-                String[] fruits = str.split("a");
-                System.out.println(fruits[5]);
-				System.out.println(fruits.length);
+		String str = "  Java Programming  ";
+                System.out.println(str.trim());
 	}
 }
