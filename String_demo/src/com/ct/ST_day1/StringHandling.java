6c61dd368d319404b6da8722d7b2cea8f7de6510
chetan-there <therechetan24@gmail.com>
2025-05-08 12:01:00 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -1,8 +1,10 @@
 package com.ct.ST_day1;
-
 public class StringHandling {
-	public static void main(String[] args) {
-		String str = "NareshIT";
-		System.out.println(str+=10);
+	public static void main(String[] args) 
+	{
+		String str = "apple,banana,orange";
+                String[] fruits = str.split("a");
+                System.out.println(fruits[5]);
+				System.out.println(fruits.length);
 	}
 }
