864cd7c0a26ec44a711f15bf9c52f8f715c1451e
chetan-there <therechetan24@gmail.com>
2025-05-08 11:48:08 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -1,10 +1,8 @@
 package com.ct.ST_day1;
 
 public class StringHandling {
-    public static void main(String[] args) {
-        String str = "NareshIT";
-        char ch[] = { 'N', 'a', 'r', 'e', 's', 'h', 'I', 'T' };
-        String str1 = new String(ch);
-        System.out.println(str.length() + str1.length());
-    }
+	public static void main(String[] args) {
+		String str = "Naresh I Technologies".replace("e", "E");
+		System.out.println(str);
+	}
 }
