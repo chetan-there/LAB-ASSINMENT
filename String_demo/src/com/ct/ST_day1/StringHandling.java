45beb910082248c90cbb8ebcbdc19171bbe19832
chetan-there <therechetan24@gmail.com>
2025-05-08 12:15:05 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -1,10 +1,8 @@
 package com.ct.ST_day1;
 public class StringHandling {
-	public static void main(String[] args) {
-		String str = "Naresh I Technologies Java";
-		for(int i=0;i<str.length();i++)
-		{			
-			System.out.print(str.charAt(i)+" ");
-		}
+	public static void main(String[] args) 
+	{
+		String str = " ";
+                System.out.println(str.isBlank());
 	}
 }
