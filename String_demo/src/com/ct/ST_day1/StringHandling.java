2dfcc0bd5d8ded9b5eecaf7abcf8116e40fd2d7a
chetan-there <therechetan24@gmail.com>
2025-05-08 11:23:32 +0530

    ST_day1

---
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -0,0 +1,10 @@
+package com.ct.ST_day1;
+
+public class StringHandling {
+	public static void main(String[] args) {
+		String str = "NareshIT";
+		String str1 = "NareshIT";
+		System.out.println(str == str1);
+		System.out.println(str.equals(str1));
+	}
+}
