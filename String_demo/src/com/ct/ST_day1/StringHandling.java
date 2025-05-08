6bcf0e97a89ed3b78bb08d5c6c79830a5e5b4f22
chetan-there <therechetan24@gmail.com>
2025-05-08 11:50:14 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -2,7 +2,7 @@ package com.ct.ST_day1;
 
 public class StringHandling {
 	public static void main(String[] args) {
-		String str = "Naresh I Technologies".replace("e", "E");
-		System.out.println(str);
+		String str = "NareshIT";
+		System.out.println(str+=10);
 	}
 }
