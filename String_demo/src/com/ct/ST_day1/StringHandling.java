421821a048ffc27e88eaabb6c136cbeda3c6d46f
chetan-there <therechetan24@gmail.com>
2025-05-08 11:26:26 +0530

    ST_day1

--- String_demo/src/com/ct/ST_day1/StringHandling.java
+++ String_demo/src/com/ct/ST_day1/StringHandling.java
@@ -1,10 +1,10 @@
 package com.ct.ST_day1;
 
 public class StringHandling {
-	public static void main(String[] args) {
-		String str = "NareshIT";
-		String str1 = "NareshIT";
-		System.out.println(str == str1);
-		System.out.println(str.equals(str1));
-	}
+    public static void main(String[] args) {
+        String str = "NareshIT";
+        char ch[] = { 'N', 'a', 'r', 'e', 's', 'h', 'I', 'T' };
+        String str1 = new String(ch);
+        System.out.println(str.length() + str1.length());
+    }
 }
