2f050d1031f8757759d5bb1664a9f8b182afdd0b
chetan-there <therechetan24@gmail.com>
2025-05-18 00:18:56 +0530

    strak

--- MOCK/src/com/ct/logical/prime_demo.java
+++ MOCK/src/com/ct/logical/prime_demo.java
@@ -1,15 +1,41 @@
 package com.ct.logical;
 
-import java.util.Scanner;
-
 public class prime_demo {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        Boolean flag = false;
 
-        if(num ==1 )
+        int flag = 0;
 
+        int n = 5;
+
+        if (n == 0 || n == 1) {
+
+            System.out.println("0 and 1 not considered as prime number");
+
+        } else {
+
+            for (int i = 2; i <= n / 2; i++) {
+
+                if (n % i == 0) {
+
+                    flag = 1;
+
+                }
+
+            }
+
+            if (flag == 0) {
+
+                System.out.println(n + " Given number is prime");
+
+            } else {
+
+                System.out.println(n + " Not prime");
+
+            }
+
+        }
 
     }
+
 }
