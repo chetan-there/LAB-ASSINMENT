97db7cc3c05bc8ac72d7e251fed9117c55add908
chetan-there <therechetan24@gmail.com>
2025-05-18 22:59:30 +0530

    inital

--- MOCK/src/com/ct/logical/reverse.java
+++ MOCK/src/com/ct/logical/reverse.java
@@ -5,9 +5,7 @@ public class reverse {
     public static void main(String[] args) {
 
         int num = 12345;
-
         int res = 0;
-
         int rem = 0;
 
         while (num > 0) {
@@ -17,9 +15,7 @@ public class reverse {
             res = (res * 10) + rem;
 
             num = num / 10;
-
         }
-
         System.out.println("Reverse of a number: " + res);
 
     }
