d7445b765dde41ab466ea91ca198102af1dd241a
chetan-there <therechetan24@gmail.com>
2025-05-22 12:30:32 +0530

    Collection day2

---
+++ Collection_demo/src/com/ct/collection_day2/test.java
@@ -0,0 +1,27 @@
+class Emp 
+{
+	private int age ;
+	
+	public Emp( int age )
+	{
+		super();
+		this.age = age;
+	}
+	
+	public int hashCode()
+	{
+		return age;
+	}
+}
+public class test
+{
+	public static void main(String[] args) 
+	{
+		Emp emp1 = new Emp(23);
+		System.out.println("emp1.hashCode() is :"+emp1.hashCode());
+
+
+		int originalHashCode = System.identityHashCode(emp1);
+		System.out.println("Original hashCode of Emp is :"+originalHashCode);
+	}
+}
